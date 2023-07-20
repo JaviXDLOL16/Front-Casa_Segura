bbc030a75c75122a48177bf3a94a37d85871a02f
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2023-07-20 06:18:31 -0600

    save data

--- screens/MainMenu.js
+++ screens/MainMenu.js
@@ -1,9 +1,10 @@
 
 import {Text, View,SafeAreaView } from 'react-native';
 import { useFonts } from "expo-font";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect,useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { useNavigation } from "@react-navigation/native";
+import * as Location from 'expo-location';
 
 
 import SectionIndicator from '../components/SectionIndicator';
@@ -12,13 +13,57 @@ import DashboardButton from '../components/DashboardButton';
 import Task from '../components/Task';
 import { ScrollView } from 'native-base';
 
+
+const OPEN_WEATHER_MAP_API_KEY = 'cf94987af5ec0c9d23b084489d6d5d28'; // Reemplaza con tu clave de API
+const GOOGLE_MAPS_API_KEY = 'AIzaSyAZwrSq95phl6DV2ulGjvQKUHPAbqJmpME'; // Reemplaza con tu clave de API
+
 export default function MainMenu() {
+  const [location, setLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [placeDetails, setPlaceDetails] = useState(null);
+//Letras
     const navigation = useNavigation();
     const [fontsLoaded] = useFonts({
         MulishBold: require("../assets/fonts/Mulish-Bold.ttf"),
         MulishLight: require("../assets/fonts/Mulish-Light.ttf"),
       });
     
+
+      useEffect(() => {
+        (async () => {
+          try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+              console.log('Permiso de ubicación denegado');
+              return;
+            }
+    
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+    
+            // Obtener el nombre del lugar utilizando la API de Google Maps
+            let response = await fetch(
+              `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.coords.latitude},${location.coords.longitude}&key=${GOOGLE_MAPS_API_KEY}`
+            );
+            let data = await response.json();
+    
+            if (data.results && data.results.length > 0) {
+              setPlaceDetails(parsePlaceDetails(data.results[0]));
+    
+              // Obtener la información meteorológica de acuerdo a la longitud y latitud
+              const weatherResponse = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_MAP_API_KEY}`
+              );
+              const weatherData = await weatherResponse.json();
+    
+              setWeatherData(weatherData);
+            }
+          } catch (error) {
+            console.log('Error al obtener la ubicación o los datos meteorológicos:', error);
+          }
+        })();
+      }, []);
+
       useEffect(() => {
         async function prepare() {
           await SplashScreen.preventAutoHideAsync();
@@ -37,6 +82,34 @@ export default function MainMenu() {
       const handleButtonPress = (onPress) => {
         navigation.navigate(onPress);
       };
+
+
+      //Clima y lugar
+
+
+    
+      // Función para obtener los detalles del lugar a partir del resultado de la API de Google Maps
+      const parsePlaceDetails = (result) => {
+        let placeDetails = {
+          municipio: '',
+          estado: '',
+          pais: ''
+        };
+    
+        const addressComponents = result.address_components;
+        for (let i = 0; i < addressComponents.length; i++) {
+          const types = addressComponents[i].types;
+          if (types.includes('locality')) {
+            placeDetails.municipio = addressComponents[i].long_name;
+          } else if (types.includes('administrative_area_level_1')) {
+            placeDetails.estado = addressComponents[i].long_name;
+          } else if (types.includes('country')) {
+            placeDetails.pais = addressComponents[i].long_name;
+          }
+        }
+    
+        return placeDetails;
+      };
     
   return (
 
@@ -45,13 +118,22 @@ export default function MainMenu() {
 <SectionIndicator
 Texto={"Menu principal"}
 />
-
+{weatherData && placeDetails ? (
 <Wheather
-Lugar={"Tuxtla Gutierrez"}
-Max={"38°"}
-Min={"42°"}
-Temperatura={"65°"}
+Lugar={placeDetails.municipio}
+Max={weatherData.main.temp_max.toFixed(0)-268}
+Min={weatherData.main.temp_min.toFixed(0)-276}
+Temperatura={weatherData.main.temp.toFixed(0)-273}
 />
+
+) : (
+  <Wheather
+Lugar={"--------"}
+Max={"--°"}
+Min={"--°"}
+Temperatura={"--°"}
+/>
+)}
 
 <View className="h-[60%] justify-center">
    <DashboardButton
