37abf5b5a07e62da0e9dd78c3d55d769e0de8d19
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2023-07-20 09:59:50 -0600

    save changes 2.0

--- screens/Gas.js
+++ screens/Gas.js
@@ -1,7 +1,10 @@
-import {Text, View,ScrollView } from 'react-native';
+import {Text, View,ScrollView, Button, Platform  } from 'react-native';
 import { useFonts } from "expo-font";
 import { useCallback, useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
+import { useState,useRef } from 'react';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 
 import SectionIndicator from '../components/SectionIndicator';
@@ -9,8 +12,20 @@ import Task from '../components/Task';
 import Circle from '../components/Circle';
 
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 
 export default function Gas() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
     const [fontsLoaded] = useFonts({
         MulishBold: require("../assets/fonts/Mulish-Bold.ttf"),
         MulishLight: require("../assets/fonts/Mulish-Light.ttf"),
@@ -23,6 +38,23 @@ export default function Gas() {
         prepare();
       }, []);
     
+      useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+          setNotification(notification);
+        });
+    
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+          console.log(response);
+        });
+    
+        return () => {
+          Notifications.removeNotificationSubscription(notificationListener.current);
+          Notifications.removeNotificationSubscription(responseListener.current);
+        };
+      }, []);
+
       const onLayout = useCallback(async () => {
         if (fontsLoaded) {
           await SplashScreen.hideAsync();
@@ -43,10 +75,61 @@ Texto={"Monitor de humo"}
 Informacion={"Todo se encuentra en orden"}
 Datos={"Si"}
 />
-
 </ScrollView>
+<Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+/>
 <Task></Task>
  </View>
 
   );
+}
+
+
+
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Leo es mampo y se besa con el diego",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 }
--- screens/MainMenu.js
+++ screens/MainMenu.js
@@ -7,11 +7,11 @@ import { useNavigation } from "@react-navigation/native";
 import * as Location from 'expo-location';
 
 
+
 import SectionIndicator from '../components/SectionIndicator';
 import Wheather from '../components/Weather';
 import DashboardButton from '../components/DashboardButton';
 import Task from '../components/Task';
-import { ScrollView } from 'native-base';
 
 
 const OPEN_WEATHER_MAP_API_KEY = 'cf94987af5ec0c9d23b084489d6d5d28'; // Reemplaza con tu clave de API
@@ -20,7 +20,9 @@ const GOOGLE_MAPS_API_KEY = 'AIzaSyAZwrSq95phl6DV2ulGjvQKUHPAbqJmpME'; // Reempl
 export default function MainMenu() {
   const [location, setLocation] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
-  const [placeDetails, setPlaceDetails] = useState(null);
+  const [municipio, setMunicipio] = useState(null);
+  const [estado, setEstado] = useState(null);
+  const [pais, setPais] = useState(null);
 //Letras
     const navigation = useNavigation();
     const [fontsLoaded] = useFonts({
@@ -48,13 +50,21 @@ export default function MainMenu() {
             let data = await response.json();
     
             if (data.results && data.results.length > 0) {
-              setPlaceDetails(parsePlaceDetails(data.results[0]));
+              const placeDetails = data.results[0].address_components;
+              const municipio = placeDetails.find((component) => component.types.includes('locality')).long_name;
+              const estado = placeDetails.find((component) => component.types.includes('administrative_area_level_1')).long_name;
+              const pais = placeDetails.find((component) => component.types.includes('country')).long_name;
+    
+              setMunicipio(municipio);
+              setEstado(estado);
+              setPais(pais);
     
               // Obtener la información meteorológica de acuerdo a la longitud y latitud
               const weatherResponse = await fetch(
                 `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_MAP_API_KEY}`
               );
               const weatherData = await weatherResponse.json();
+              console.log('Datos meteorológicos:', weatherData);
     
               setWeatherData(weatherData);
             }
@@ -83,34 +93,6 @@ export default function MainMenu() {
         navigation.navigate(onPress);
       };
 
-
-      //Clima y lugar
-
-
-    
-      // Función para obtener los detalles del lugar a partir del resultado de la API de Google Maps
-      const parsePlaceDetails = (result) => {
-        let placeDetails = {
-          municipio: '',
-          estado: '',
-          pais: ''
-        };
-    
-        const addressComponents = result.address_components;
-        for (let i = 0; i < addressComponents.length; i++) {
-          const types = addressComponents[i].types;
-          if (types.includes('locality')) {
-            placeDetails.municipio = addressComponents[i].long_name;
-          } else if (types.includes('administrative_area_level_1')) {
-            placeDetails.estado = addressComponents[i].long_name;
-          } else if (types.includes('country')) {
-            placeDetails.pais = addressComponents[i].long_name;
-          }
-        }
-    
-        return placeDetails;
-      };
-    
   return (
 
     <View className="flex-1 bg-[#F0F0F2] items-center justify-start" onLayout={onLayout}>
@@ -118,9 +100,9 @@ export default function MainMenu() {
 <SectionIndicator
 Texto={"Menu principal"}
 />
-{weatherData && placeDetails ? (
+{weatherData && municipio ? (
 <Wheather
-Lugar={placeDetails.municipio}
+Lugar={municipio}
 Max={weatherData.main.temp_max.toFixed(0)-268}
 Min={weatherData.main.temp_min.toFixed(0)-276}
 Temperatura={weatherData.main.temp.toFixed(0)-273}
